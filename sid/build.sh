ec8afe68c5f213c179e17353f17565393d39f223
HaiBac Ngo <ngohaibac@gmail.com>
2015-07-22 22:52:21 +0700

    change image name to docker-armel

--- sid/build.sh
+++ sid/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo ../mkimage.sh -d . -t $USER/debian-minbase:sid debootstrap --variant=minbase --components=main --include=inetutils-ping,iproute2 --force-check-gpg  sid http://httpredir.debian.org/debian > build.log 2>&1
+sudo ../mkimage.sh -d . -t $USER/debian-armel:sid debootstrap --variant=minbase --components=main --include=inetutils-ping,iproute2 --force-check-gpg  sid http://httpredir.debian.org/debian > build.log 2>&1
 
