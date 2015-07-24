bf9d6241283da075fe247739712b4a3bdabe7553
HaiBac Ngo <ngohaibac@gmail.com>
2015-07-24 23:54:04 +0700

    correct command in sid

--- sid/build.sh
+++ sid/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo ../mkimage.sh -d . -t $USER/debian-armel:sid debootstrap --arch=armel--variant=minbase --components=main --include=inetutils-ping,iproute2 --force-check-gpg  sid http://httpredir.debian.org/debian > build.log 2>&1
+sudo ../mkimage.sh -d . -t $USER/debian-armel:sid debootstrap --arch=armel --variant=minbase --components=main --include=inetutils-ping,iproute2 --force-check-gpg  sid http://httpredir.debian.org/debian > build.log 2>&1
 
